de0f26d27feeb9a25676dece8be58cf0ea12b5b6
Becker <beckerlucas19@gmail.com>
2023-09-09 16:44:27 -0300

    feat: creating header component

---
+++ capputeeno/src/components/cart-control.tsx
@@ -0,0 +1,30 @@
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { CartIcon } from "./cart-icon";
+import { styled } from "styled-components";
+
+const CartCount = styled.span`
+    width: 17px;
+    height: 17px;
+    padding: 0 5px;
+    margin-left: -10px;
+    border-radius: 50%;
+
+    font-size: 10px;
+    color: white;
+    background-color: var(--danger-color);
+`;
+
+const Container = styled.div`
+    position: relative;
+`;
+
+export function CartControl() {
+    const { value } = useLocalStorage('cart-items');
+
+    return (
+        <Container>
+            <CartIcon/>
+            {value.length && <CartCount>{value.length}</CartCount>}
+        </Container>
+    )
+}

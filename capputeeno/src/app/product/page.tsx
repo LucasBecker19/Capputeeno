f859952c2d403461fbd172a75b6f8f419e206237
Becker <beckerlucas19@gmail.com>
2023-09-24 17:53:06 -0300

    feat: add products to cart

--- capputeeno/src/app/product/page.tsx
+++ capputeeno/src/app/product/page.tsx
@@ -106,6 +106,36 @@ const ProductInfo = styled.div`
 export default function Product({ searchParams }: { searchParams: { id: string } }) {
     const { data } = useProduct(searchParams.id);
     
+    const handleAddToCart = () => {
+        const cartItems = localStorage.getItem('cart-items');
+
+        if(cartItems) {
+            const cartItemsArray = JSON.parse(cartItems);
+
+            const existingProductIndex = cartItemsArray.findIndex((item: { id: string; }) => item.id === searchParams.id);
+            
+            if(existingProductIndex != -1) {
+                cartItemsArray[existingProductIndex].quantity += 1; 
+            } else {
+                cartItemsArray.push({ 
+                    ...data, 
+                    id: searchParams.id, 
+                    quantity: 1 
+                });
+            }
+
+            localStorage.setItem('cart-items', JSON.stringify(cartItemsArray));
+        } else {
+            const newCart = [{
+                ...data,
+                id: searchParams.id,
+                quantity: 1
+            }];
+
+            localStorage.setItem('cart-items', JSON.stringify(newCart));
+        }
+    }
+
     return (
         <DefaultPageLayout>
             <Container>
@@ -127,7 +157,7 @@ export default function Product({ searchParams }: { searchParams: { id: string }
                             </div>
                         </ProductInfo>
 
-                        <button>
+                        <button onClick={handleAddToCart}>
                             <ShoppingBagIcon/>
                             Adicionar ao carrinho
                         </button>
